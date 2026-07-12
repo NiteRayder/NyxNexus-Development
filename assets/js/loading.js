45f4ec95c4fcaca43617d8622eb3946d1bc9255e
CodeRayder <BrooklynDeWolf@proton.me>
2026-07-12 08:55:53 -0400

    Add loading.js to handle page loading animation

---
+++ assets/js/loading.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+        loader.remove();
+    }, 800);
+
+});
